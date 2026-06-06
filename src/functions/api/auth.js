ea5d5d9cb92fc1e77b53a6c54493219ba2fbfc9a
intirupati <javesh8118@gmail.com>
2026-06-06 21:25:41 +0530

    Update auth.js

--- src/functions/api/auth.js
+++ src/functions/api/auth.js
@@ -8,7 +8,7 @@ export async function onRequest(context) {
         data, // arbitrary space for passing data between middlewares
     } = context;
 
-    const client_id = env.GITHUB_CLIENT_ID;
+    const client_id = env.Ov23lixNJvKXTLIG3O4t;
 
     try {
         const url = new URL(request.url);
